6e90259b020b05685bf2668c9633021af123b669
rollomkhitaryan-wq <rollomkhitaryan@gmail.com>
2026-07-09 14:54:31 +0400

    Homework

--- src/HomeWork2.java
+++ src/HomeWork2.java
@@ -1,4 +1,89 @@
-package PACKAGE_NAME;
-
 public class HomeWork2 {
+    public static void main(String[] args){
+        //InClass
+
+        int rows = 5;
+        int elements = 1;
+        for(int i = 1; i<=rows; i++){
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        //Homework
+
+        for (int i = 1; i <=rows; i++) {
+            for (int j = 0; j < rows - i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+        //Ex.1
+
+        int [] task1 = new int[]{2, 2, 2};
+
+        System.out.println(task1[0]);
+
+        //Ex.2
+
+        int [] task2 = new int[1000];
+        for(int i = 0; i < task2.length; ++i){
+            task2[i] = i + 1;
+        }
+        for (int i = 0; i < task2.length; i++){
+            System.out.println(task2[i]);
+        }
+
+        //Ex.3
+
+        int [] task3 = new int[20];
+        int index = 0;
+        for (int i = -19; i < 20;  i+=2) {
+            task3[index++] = i;
+
+        }
+        for (int i = 0; i < task3.length ; i++) {
+            System.out.println(task3[i]);
+
+        }
+
+        //Ex.4
+
+        int [] task4 = new int [75];
+        int index4 = 0;
+        for (int i = 0; i <=75 ; i+=5) {
+            task4[index4++] = i;
+            System.out.println(i);
+
+        }
+
+        //Ex.5
+
+        float [] task5 = new float[468];
+        int index5 = 0;
+        for (int i = 0; i < 468; i++) {
+            task5[index5++] = i;
+
+            if(i > 24.12 && i < 467.23){
+                System.out.println(i);
+            }
+
+        }
+
+        //Ex.7
+
+        int n = 3;
+        int [] task7 = new int[11];
+        int index7 = 0;
+        for (int i = 0; i < 11 ; i++) {
+            task7[index7++] = i;
+
+            System.out.println( "3"+ " " + "*"  + " " + i + " " + "=" + " "  + n * i );
+
+        }
+    }
 }
